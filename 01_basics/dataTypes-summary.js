93feabd90759aa4b78f19c3d4e5747f0886e67d4
Siddhartha Mukherjee <104203643+Siddhartha-0709@users.noreply.github.com>
2023-12-17 21:59:51 +0000

    datatypes summary

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,35 @@
+// #Primitive Datatypes
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+//JavaScript is a dynamically typed language. In dynamically typed languages, the data type of variables is determined by the value they hold at runtime, and can change throughout the program as we assign different values to them. There's no inbuilt way to annotate or restrict the types of variables.
+
+// #Reference Datatypes or Non Primitive
+
+// Arrays, Object, Functions
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+console.log(typeof(myFunction))
+console.log(typeof anotherId);
